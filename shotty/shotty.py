638a015fe82a7bf3e6ff3dbba5057d174ca10c5b
Dariush Azimi <azimid@gmail.com>
2018-11-01 13:26:54 -0400

    updated script to use click module, and show more info about instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,10 +4,29 @@
 # pipenv install pylint --d 
 
 import boto3
+import click
+# import os
+
+session = boto3.Session(profile_name='snapshotty')
+ec2 = session.resource('ec2')
+
+
+@click.command()
+def list_instances():
+    '''
+    List Ec2 instances
+    '''
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name
+        )))
+
 
 if __name__ == '__main__':
+    # print(sys.argv)
 
-    session = boto3.Session(profile_name='snapshotty')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
