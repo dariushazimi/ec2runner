1a2d4e45b1648decd23010d1fcf3d237c9aa7d6c
Dariush Azimi <azimid@gmail.com>
2018-11-01 15:57:24 -0400

    added click group command and stop function to stop Ec2 instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -13,18 +13,30 @@ session = boto3.Session(profile_name='snapshotty')
 ec2 = session.resource('ec2')
 
 
-@click.command()
+def filter_instances(project):
+    instances = []
+    
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    
+    return instances
+
+
+# Grouping some of the commands together using click
+@click.group()
+def instances():
+    """ Commands for instances"""
+@instances.command('list')
 @click.option('--project', default=None,
                 help="only intances for project (tag Project=<name>)")
 def list_instances(project):
     '''
     List ec2 instances
     '''
-    if project:
-        filters = [{'Name': 'tag:Project', 'Values': [project]}]
-        instances = ec2.instances.filter(Filters=filters)
-    else:
-        instances = ec2.instances.all()
+    instances = filter_instances(project)
 
     for i in instances:
         # if there are no tags, retrun an empty list 'or []'
@@ -38,6 +50,22 @@ def list_instances(project):
             tags.get('Project', '<no project>')
         )))
 
+    return
+
+@instances.command('stop')
+@click.option('--project', default=None, help="only instance for project (tag Project=<name>")
+def stop_instances(project):
+    "Stop Ec2 instances"
+    
+    instances = filter_instances(project)
+    
+    for i in instances:
+        print("Stopping {0}... ".format(i.id))
+        i.stop()
+    return
+
+
+
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
